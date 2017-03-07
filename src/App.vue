c4766afb04796f7988b4acf16214c36529e77384
annhuang <397401374@qq.com>
2017-03-07 12:26:46 +0800

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
     <router-view></router-view>
+    <div class="header">
+      I am header
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app'
-}
+};
 </script>
 
 <style>
@@ -16,8 +24,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
+// new对象时，必须把该对象赋给一个变量
+// 单独给下面代码注释，可以跳过eslint规则校验
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
-})
+});
