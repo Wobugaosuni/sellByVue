6e224ff4dcf1398ebadab0f55cae40b5298f69c8
annhuang <397401374@qq.com>
2017-03-14 13:45:41 +0800

    header增加过渡效果和背景模糊

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,38 +31,43 @@
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background" :style="'background-image: url(' + seller.avatar +')'"></div>
-    <div class="detail" v-show="detailShow">
-      <div class="detail-wrapper">
-        <h3 class="name">
-          {{seller.name}}
-        </h3>
-        <div class="star-wrapper">
-          <star :score="seller.score" :size='48'></star>
-        </div>
-        <div class="title">
-          <div class="line"></div>
-          <div class="text">优惠信息</div>
-          <div class="line"></div>
-        </div>
-        <div class="previlege-wrapper" v-if="seller.supports">
-          <div v-for="item in seller.supports" class="previlege">
-            <span :class="'icon ' + bgImageMap[item.type]"></span>
-            <span class="text">{{item.description}}</span>
+
+    <transition name="detail-transition">
+      <div class="detail" v-show="detailShow">
+        <div class="detail-wrapper">
+          <h3 class="name">
+            {{seller.name}}
+          </h3>
+          <div class="star-wrapper">
+            <star :score="seller.score" :size='48'></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul class="supports" v-if="seller.supports">
+            <li v-for="item in seller.supports" class="support-item">
+              <span :class="'icon ' + bgImageMap[item.type]"></span>
+              <span class="text">{{item.description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin-container">
+            <p>
+              {{seller.bulletin}}
+            </p>
           </div>
         </div>
-        <div class="title">
-          <div class="line"></div>
-          <div class="text">商家公告</div>
-          <div class="line"></div>
-        </div>
-        <div class="bulletin-container">
-          {{seller.bulletin}}
+        <div class="footer-wrapper" v-on:click="onDetailHideClick">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="footer-wrapper" v-on:click="onDetailHideClick">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
