3bcb96302a5dafaacb4acce468de70bb1444b1ae
annhuang <397401374@qq.com>
2017-03-14 11:08:31 +0800

    完成弹窗开发

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -36,7 +36,28 @@
         <h3 class="name">
           {{seller.name}}
         </h3>
-
+        <div class="star-wrapper">
+          <star :score="seller.score" :size='48'></star>
+        </div>
+        <div class="title">
+          <div class="line"></div>
+          <div class="text">优惠信息</div>
+          <div class="line"></div>
+        </div>
+        <div class="previlege-wrapper" v-if="seller.supports">
+          <div v-for="item in seller.supports" class="previlege">
+            <span :class="'icon ' + bgImageMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </div>
+        </div>
+        <div class="title">
+          <div class="line"></div>
+          <div class="text">商家公告</div>
+          <div class="line"></div>
+        </div>
+        <div class="bulletin-container">
+          {{seller.bulletin}}
+        </div>
       </div>
       <div class="footer-wrapper" v-on:click="onDetailHideClick">
         <i class="icon-close"></i>
@@ -46,196 +67,34 @@
 </template>
 
 <script type='text/ecmascript-6'>
-  export default {
-    props: ['seller'],
-    data () {
-      return {
-        detailShow: false
-      };
+import star from '../star/star';
+export default {
+  components: {
+    star
+  },
+  props: ['seller'],
+  data () {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    onDetailShowClick: function () {
+      this.detailShow = true;
     },
-    methods: {
-      onDetailShowClick: function () {
-        this.detailShow = true;
-      },
-      onDetailHideClick: function () {
-        this.detailShow = false;
-      }
-    },
-    created () {
-      this.bgImageMap = ['discount', 'decrease', 'special', 'invoice', 'guarantee'];
+    onDetailHideClick: function () {
+      this.detailShow = false;
     }
-  };
+  },
+  created () {
+    this.bgImageMap = ['discount', 'decrease', 'special', 'invoice', 'guarantee'];
+  }
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
 
 @import '../../common/stylus/mixin.styl';
+@import 'header.styl';
 
-  .header
-    position: relative
-    color: rgb(255,255,255)
-    background: rgba(7,17,27,0.5)
-    overflow: hidden
-
-    .content-wrapper
-      position: relative
-      padding: 24px 12px 18px 24px
-      font-size: 0  // 消除子元素inline-block造成的空白间隙
-
-      .avatar
-        display: inline-block
-        margin-right: 16px
-        vertical-align: top
-
-        > img
-          border-radius: 2px
-
-      .content
-        display: inline-block
-        padding-top: 2px
-        padding-bottom: 2px
-
-        .title
-          font-size: 0
-          margin-bottom: 8px
-
-          .brand
-            display: inline-block
-            width: 30px
-            height: 18px
-            margin-right: 6px
-            bgImage('brand')
-            background-size: 30px 18px
-            background-repeat: no-repeat
-            vertical-align: top
-
-          .name
-            font-size: 16px
-            font-weight: bold
-            line-height: 18px
-
-        .description
-          font-size: 12px
-          margin-bottom: 10px
-
-        .support
-          font-size: 10px
-
-          .icon
-            display: inline-block
-            width: 12px
-            height: 12px
-            margin-right: 4px
-            vertical-align: top
-            background-size: 12px 12px
-            background-repeat: no-repeat
-
-            &.decrease
-              bgImage('decrease_1')
-
-            &.discount
-              bgImage('discount_1')
-
-            &.guarantee
-              bgImage('guarantee_1')
-
-            &.invoice
-              bgImage('invoice_1')
-
-            &.special
-              bgImage('special_1')
-
-          .text
-            line-height: 12px
-
-      .support-count
-        position: absolute
-        right: 12px
-        bottom: 18px
-        padding: 7px 8px
-        background-color: rgba(0,0,0,0.2)
-        border-radius: 14px
-        font-size: 10px
-
-        .count
-          line-height: 12px
-          vertical-align: top
-          margin-right: 2px
-
-        .icon-keyboard_arrow_right
-          line-height: 12px
-
-    .bulletin-wrapper
-      position: relative
-      height: 28px
-      line-height: 28px
-      padding: 0 22px 0 12px
-      background-color: rgba(7,17,27,0.2)
-      overflow: hidden
-      white-space: nowrap
-      text-overflow: ellipsis
-      font-size: 10px
-
-      .bulletin-title
-        display: inline-block
-        width: 22px
-        height: 12px
-        vertical-align: top
-        margin-top: 7px
-        background-size: 22px 12px
-        background-repeat: no-repeat
-        bgImage('bulletin')
-
-      .bulletin-text
-        margin: 0 4px
-        vertical-align: top
-
-      .icon-keyboard_arrow_right
-        position: absolute
-        right: 12px
-        line-height: inherit
-
-    .background
-      position: absolute
-      left: 0
-      top: 0
-      z-index: -1
-      width: 100%
-      height: 100%
-      background-size: cover
-      filter: blur(10px)  // 设置背景模糊
-
-/*
- * detail弹窗使用sticky footers布局: https://www.w3cplus.com/css3/css-secrets/sticky-footers.html
- */
-    .detail
-      position: fixed
-      top: 0
-      left: 0
-      z-index: 100
-      width: 100%
-      height: 100%
-      overflow: auto
-      background-color: rgba(7,17,27,0.8)
-
-      .detail-wrapper
-        box-sizing: border-box
-        min-height: 100%
-        padding: 64px 36px 64px
-
-        .name
-          margin: 0
-          padding: 0
-          font-size: 16px
-          font-weight: 700
-          line-height: 16px
-          text-align: center
-
-      .footer-wrapper
-        margin-top: -64px
-        text-align: center
-
-        .icon-close
-          font-size: 32px
-          color: rgba(255,255,255,0.5)
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,104 @@
+<template>
+  <div :class="'star ' + starType">
+    <span v-for="itemClass in itemClasses" :class="'star-item ' + itemClass"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const LENGTH = 5;
+  const CLS_ON = 'on';
+  const CLS_OFF = 'off';
+  const CLS_HALF = 'half';
+
+  export default {
+    props: ['size', 'score'],
+    computed: {
+      starType: function () {
+        return 'star-' + this.size;
+      },
+      itemClasses () {
+        let result = [];
+        let score = Math.floor(this.score * 2) / 2;
+        let integer = Math.floor(score);
+        let hasDecimals = score % 1 !== 0;
+        for (let i = 0; i < integer; i++) {
+          result.push(CLS_ON);
+        }
+        if (hasDecimals) {
+          result.push(CLS_HALF);
+        }
+        while (result.length < LENGTH) {
+          result.push(CLS_OFF);
+        }
+        return result;
+      }
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin.styl';
+
+  .star
+    // font-size: 0
+    text-align: center
+
+    .star-item
+      display: inline-block
+      background-repeat: no-repeat
+
+      &:last-child
+        margin-right: 0
+
+  &.star-24
+
+    .star-item
+      width: 10px
+      height: 10px
+      background-size: 10px 10px
+      margin-right: 3px
+
+      &.on
+        bgImage('star24_on')
+
+      &.half
+        bgImage('star24_half')
+
+      &.off
+        bgImage('star24_off')
+
+  &.star-36
+
+    .star-item
+      width: 15px
+      height: 15px
+      background-size: 15px 15px
+      margin-right: 7px
+
+      &.on
+        bgImage('star36_on')
+
+      &.half
+        bgImage('star36_half')
+
+      &.off
+        bgImage('star36_off')
+
+  &.star-48
+
+    .star-item
+      width: 20px
+      height: 19px
+      background-size: 20px 19px
+      margin-right: 20px;
+
+      &.on
+        bgImage('star48_on')
+
+      &.half
+        bgImage('star48_half')
+
+      &.off
+        bgImage('star48_off')
+
+
+</style>
